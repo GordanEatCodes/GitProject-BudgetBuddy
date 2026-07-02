dadfd5cc4df9293f48d238bc0b501618c5166f77
GordanEatCodes <gordan.ng.hungzhuen@student.mmu.edu.my>
2026-07-02 15:09:29 +0800

    save

--- users/management/commands/create_admin.py
+++ users/management/commands/create_admin.py
@@ -2,6 +2,7 @@ import os
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
+
 class Command(BaseCommand):
     help = 'Create a superuser from environment variables if one does not exist'
 
@@ -15,18 +16,18 @@ class Command(BaseCommand):
                 'DJANGO_SUPERUSER_USERNAME or DJANGO_SUPERUSER_PASSWORD not set. Skipping admin creation.'
             ))
             return
-        
+
         if User.objects.filter(username=username).exists():
             self.stdout.write(self.style.SUCCESS(
                 f'Superuser "{username}" already exists. Skipping creation.'
             ))
-        return
-    
+            return
+
         User.objects.create_superuser(
-            username=username, 
-            email=email or '', 
+            username=username,
+            email=email or '',
             password=password
         )
         self.stdout.write(self.style.SUCCESS(
             f'Superuser "{username}" created successfully.'
-        ))  
+        ))
